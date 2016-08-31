e111f20f6d44316c1f3f87f4ba3a105ce8c14944
Macintoshplus <814683+macintoshplus@users.noreply.github.com>
2016-08-31 10:16:30 +0200

    Remove phpuser before start

--- bin/container-bootstrap.sh
+++ bin/container-bootstrap.sh
@@ -3,6 +3,7 @@
 # Give apache user same uid as owener of wwwroot to avoid problems on upload.
 if [ -d "/src" ]
 	then
+		userdel phpuser
 		USERID=$(stat -c "%u" /src)
 		usermod -u $USERID www-data
 fi
